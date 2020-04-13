9ba006e3c833dbea39ef9989102aa2ae1211d901
James Bowers <james@james-bowers.me>
2020-04-13 12:23:35 +0100

    creating and fetching activity dates

---
+++ cypress/integration/activity_date_spec.js
@@ -0,0 +1,111 @@
+import {createPromoter, createActivity, createActivityDate, getAccountActivityDates, getAccountActivityDatesByActivity, signOut, getStarted} from "tb-sdk"
+import Joi from "@hapi/joi"
+
+describe("Activity Date", () => {
+  beforeEach(() => {
+    cy.execute(signOut())
+    cy.execute(getStarted())
+  })
+
+  it("creates an activity date", () => {
+    const createPromoterReq = createPromoter()
+    const createActivityReq = createActivity(createPromoterReq.body.wish.promoter_id)
+    const createActivityDateReq = createActivityDate(createActivityReq.body.wish.activity_id)
+    cy.execute(createPromoterReq)
+    cy.execute(createActivityReq)
+
+    cy.execute(createActivityDateReq)
+      .its('body')
+      .then((body) => {
+        expect(body).to.deep.eq({
+          activity_date_id: createActivityDateReq.body.wish.activity_date_id,
+          activity_id: createActivityReq.body.wish.activity_id,
+          addr_line_1: null,
+          addr_line_2: null,
+          postcode: null,
+          county: null,
+          country: null,
+          goes_live: null,
+          starts: null,
+          ends: null
+        })
+      })
+
+    cy.execute(getAccountActivityDates())
+      .its('body')
+      .then((body) => {
+        expect(body).to.deep.eq({
+          [createActivityDateReq.body.wish.activity_date_id]: {
+            activity_date_id: createActivityDateReq.body.wish.activity_date_id,
+            activity_id: createActivityReq.body.wish.activity_id,
+            addr_line_1: null,
+            addr_line_2: null,
+            postcode: null,
+            county: null,
+            country: null,
+            goes_live: null,
+            starts: null,
+            ends: null
+          }
+        })
+      })
+  })
+
+  it("fetches activity date for account", () => {
+    const createPromoterReq = createPromoter()
+    const createActivityReq = createActivity(createPromoterReq.body.wish.promoter_id)
+    const createActivityDateReq = createActivityDate(createActivityReq.body.wish.activity_id)
+    cy.execute(createPromoterReq)
+    cy.execute(createActivityReq)
+    cy.execute(createActivityDateReq)
+
+    cy.execute(getAccountActivityDates())
+      .its('body')
+      .then((body) => {
+        expect(body).to.deep.eq({
+          [createActivityDateReq.body.wish.activity_date_id]: {
+            activity_date_id: createActivityDateReq.body.wish.activity_date_id,
+            activity_id: createActivityReq.body.wish.activity_id,
+            addr_line_1: null,
+            addr_line_2: null,
+            postcode: null,
+            county: null,
+            country: null,
+            goes_live: null,
+            starts: null,
+            ends: null
+          }
+        })
+      })
+  })
+
+  it("fetches activity dates for account by activity id", () => {
+    const createPromoterReq = createPromoter()
+    const createActivityReq = createActivity(createPromoterReq.body.wish.promoter_id)
+    const createActivityDateReq = createActivityDate(createActivityReq.body.wish.activity_id)
+    cy.execute(createPromoterReq)
+    cy.execute(createActivityReq)
+    cy.execute(createActivityDateReq)
+
+    cy.execute(getAccountActivityDatesByActivity(createActivityReq.body.wish.activity_id))
+      .its('body')
+      .then((body) => {
+        expect(body).to.deep.eq({
+          [createActivityDateReq.body.wish.activity_date_id]: {
+            activity_date_id: createActivityDateReq.body.wish.activity_date_id,
+            activity_id: createActivityReq.body.wish.activity_id,
+            addr_line_1: null,
+            addr_line_2: null,
+            postcode: null,
+            county: null,
+            country: null,
+            goes_live: null,
+            starts: null,
+            ends: null
+          }
+        })
+      })
+  })
+
+  it("rejects creating an activity date for an activity which is not owned by the user")
+})
