1c759471d779046974c2c60454bd98220a815046
James Bowers <james@james-bowers.me>
2020-04-13 12:01:04 +0100

    test updating an activity

--- cypress/integration/activity_spec.js
+++ cypress/integration/activity_spec.js
@@ -1,4 +1,4 @@
-import {createPromoter, createActivity, getStarted, signOut, getAccountActivities, getAccountActivitiesByPromoter} from "tb-sdk"
+import {createPromoter, createActivity, updateActivity, getStarted, signOut, getAccountActivities, getAccountActivitiesByPromoter} from "tb-sdk"
 import {createActivitySchema, getAccountActivitiesSchema} from "../schemas"
 import Joi from "@hapi/joi"
 
@@ -20,6 +20,31 @@ describe("Activity", () => {
     cy.execute(createActivityReq)
       .its('body')
       .then((body) => cy.assertValid(schema, body))
+  })
+
+  it("updates an activity", () => {
+    const createPromoterReq = createPromoter()
+    const createActivityReq = createActivity(createPromoterReq.body.wish.promoter_id)
+
+    const updateActivityReq = updateActivity({activityId: createActivityReq.body.wish.activity_id,
+      title: "My launch event",
+      description: "A very very\n\nlong description!"
+    })
+
+    cy.execute(createPromoterReq)
+    cy.execute(createActivityReq)
+    cy.execute(updateActivityReq)
+
+    cy.execute(createActivityReq)
+      .its('body')
+      .then((body) => {
+        expect(body).to.deep.eq({
+          promoter_id: createPromoterReq.body.wish.promoter_id,
+          activity_id: createActivityReq.body.wish.activity_id,
+          title: "My launch event",
+          description: "A very very\n\nlong description!"
+        })
+      })
   })
 
   it("fetches all activities for the account", () => {
@@ -49,4 +74,8 @@ describe("Activity", () => {
   })
 
   it("rejects activity creation when user does not own promoter")
+
+  it("rejects fetching activities for a promoter the account does not own")
+
+  it("rejects updating an activity which is not owned")
 })
