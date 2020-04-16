e9fbf4cf6a62a475c8168164143b70fdf219f57a
James Bowers <james@james-bowers.me>
2020-04-16 16:38:40 +0100

    test the validation of the checkout data from the client

--- cypress/integration/checkout_spec.js
+++ cypress/integration/checkout_spec.js
@@ -54,7 +54,65 @@ describe("Checkout", () => {
       })
   })
 
-  it("prevents you starting a checkout when items do not match those on the server")
+  it("prevents you starting a checkout when items do not match those on the server", () => {
+    cy.execute(getStarted())
+    const {createTicketReq, createActivityDateReq} = ticketSetup()
+    const wrongSeatId = "this-is-so-wrong.1"
+    const correctSeatId = createTicketReq.body.wish.product_id + ".1"
+    const checkoutAmount = 400
 
-  it("prevents you starting a checkout when the total amount does not match that calculated on the server")
+    cy.execute(signOut())
+    cy.execute(getStarted())
+    cy.upgradeToVerified()
+
+    const reserveSeatReq = reserveSeat(createTicketReq.body.wish.product_id)
+    cy.execute(reserveSeatReq)
+
+    const startCheckoutReq = startCheckout([wrongSeatId], checkoutAmount)
+
+    cy.execute(startCheckoutReq)
+      .then((req) => {
+        expect(req.status).to.deep.eq(400)
+        expect(req.body[correctSeatId]).to.deep.include({
+          amount: 400,
+          item_id: correctSeatId,
+          owning_shelf: createActivityDateReq.body.wish.activity_date_id,
+          product_id: createTicketReq.body.wish.product_id,
+          shareholders: {"creditor-one-two-three": 400},
+          status: "reserved",
+          title: "Early bird ticket"
+        })
+      })
+  })
+
+  it("prevents you starting a checkout when the total amount does not match that calculated on the server", () => {
+    cy.execute(getStarted())
+    const {createTicketReq, createActivityDateReq} = ticketSetup()
+    const seatId = createTicketReq.body.wish.product_id + ".1"
+    const wrongCheckoutAmount = 399
+    const correctCheckoutAmount = 400
+
+    cy.execute(signOut())
+    cy.execute(getStarted())
+    cy.upgradeToVerified()
+
+    const reserveSeatReq = reserveSeat(createTicketReq.body.wish.product_id)
+    cy.execute(reserveSeatReq)
+
+    const startCheckoutReq = startCheckout([seatId], wrongCheckoutAmount)
+
+    cy.execute(startCheckoutReq)
+      .then((req) => {
+        expect(req.status).to.deep.eq(400)
+        expect(req.body[seatId]).to.deep.include({
+          amount: 400,
+          item_id: seatId,
+          owning_shelf: createActivityDateReq.body.wish.activity_date_id,
+          product_id: createTicketReq.body.wish.product_id,
+          shareholders: {"creditor-one-two-three": correctCheckoutAmount},
+          status: "reserved",
+          title: "Early bird ticket"
+        })
+      })
+  })
 })
