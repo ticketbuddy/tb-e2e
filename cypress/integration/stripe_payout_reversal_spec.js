41bc80309b498bdb8828cc7c0ae308cde6f384b5
James Bowers <james@james-bowers.me>
2020-04-19 21:17:57 +0100

    add reminder tests for payout reversals

---
+++ cypress/integration/stripe_payout_reversal_spec.js
@@ -0,0 +1,7 @@
+describe("Stripe payout reversals", () => {
+  it("accepts a payout reversal")
+
+  it("checks idempotency of a payout reversal")
+
+  it("returns an error status if one of the payouts fail")
+})
