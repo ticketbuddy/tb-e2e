a70b5cf0a3049bc9fe8f643cfeb90769f86144a7
James Bowers <james@james-bowers.me>
2020-04-25 12:04:23 +0100

    test admitting and unadmitting a seat

--- cypress/integration/admit_and_unadmit_spec.js
+++ cypress/integration/admit_and_unadmit_spec.js
@@ -16,24 +16,101 @@ describe("Admit and un-admit seats", () => {
         const promoterPersonId = req.body.person_id
         cy.upgradeToVerified()
 
-        // verified user
-        cy.signInAs("6b54b841c15f4b93add07b0c3f0ce59d")
-        cy.execute(reserveSeat(createTicketReq.body.wish.product_id))
+        cy.secureSign(seatId)
+          .then((entryCode) => {
+            // verified user to reserve the seat
+            cy.signInAs("6b54b841c15f4b93add07b0c3f0ce59d")
+            cy.execute(reserveSeat(createTicketReq.body.wish.product_id))
 
-        // as promoter id
-        cy.signInAs(promoterPersonId)
+            // as promoter id to admit & unadmit the ticket
+            cy.signInAs(promoterPersonId)
 
-        cy.execute(admitSeat(seatId))
-        .then((req) => {
-          expect(req.status).to.eq(200)
-          expect(req.body).to.deep.eq({"admitted?": true})
-        })
+            cy.execute(admitSeat(entryCode))
+            .then((req) => {
+              expect(req.status).to.eq(200)
+              expect(req.body).to.deep.eq({"admitted?": true})
+            })
 
-        cy.execute(unAdmitSeat(seatId))
-        .then((req) => {
-          expect(req.status).to.eq(200)
-          expect(req.body).to.deep.eq({"admitted?": false})
-        })
+            cy.execute(unAdmitSeat(entryCode))
+            .then((req) => {
+              expect(req.status).to.eq(200)
+              expect(req.body).to.deep.eq({"admitted?": false})
+            })
+          })
+      })
+  })
+
+  it("rejects invalid signed seat id", () => {
+    const {createTicketReq} = ticketSetup()
+    const seatId = createTicketReq.body.wish.product_id + ".1"
+    const entryCode = "this-is-not-valid"
+
+    cy.execute(getCurrentPerson())
+      .then((req) => {
+        const promoterPersonId = req.body.person_id
+        cy.upgradeToVerified()
+
+            // verified user to reserve the seat
+            cy.signInAs("6b54b841c15f4b93add07b0c3f0ce59d")
+            cy.execute(reserveSeat(createTicketReq.body.wish.product_id))
+
+            // as promoter id to admit & unadmit the ticket
+            cy.signInAs(promoterPersonId)
+
+            cy.execute(admitSeat(entryCode))
+            .then((req) => {
+              expect(req.status).to.eq(403)
+              expect(req.body).to.deep.eq({
+                status: "Invalid seat signature."
+              })
+            })
+
+            cy.execute(unAdmitSeat(entryCode))
+            .then((req) => {
+              expect(req.status).to.eq(403)
+              expect(req.body).to.deep.eq({
+                status: "Invalid seat signature."
+              })
+            })
+      })
+  })
+
+  it("rejects unauthorised verified user admitting seats", () => {
+    const {createTicketReq} = ticketSetup()
+    const seatId = createTicketReq.body.wish.product_id + ".1"
+
+    cy.execute(getCurrentPerson())
+      .then((req) => {
+        const promoterPersonId = req.body.person_id
+        cy.upgradeToVerified()
+
+        cy.secureSign(seatId)
+          .then((entryCode) => {
+            // verified user to reserve the seat
+            cy.signInAs("6b54b841c15f4b93add07b0c3f0ce59d")
+            cy.execute(reserveSeat(createTicketReq.body.wish.product_id))
+
+            // as random verified user
+            cy.execute(signOut())
+            cy.execute(getStarted())
+            cy.upgradeToVerified()
+
+            cy.execute(admitSeat(entryCode))
+            .then((req) => {
+              expect(req.status).to.eq(403)
+              expect(req.body).to.deep.eq({
+                status: "Not authorised to manage this seat."
+              })
+            })
+
+            cy.execute(unAdmitSeat(entryCode))
+            .then((req) => {
+              expect(req.status).to.eq(403)
+              expect(req.body).to.deep.eq({
+                status: "Not authorised to manage this seat."
+              })
+            })
+          })
       })
   })
 })
