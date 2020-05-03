7fc322d763a16152f1988525ce8651a6cfa65fb7
James Bowers <james@james-bowers.me>
2020-05-03 10:34:14 +0100

    creating a promoter

--- cypress/integration/ui/create-promoter.spec.js
+++ cypress/integration/ui/create-promoter.spec.js
@@ -1,8 +1,16 @@
+import {signOut, getStarted} from "tb-sdk"
+
 describe("Promoter", () => {
   beforeEach(() => {
     cy.execute(signOut())
     cy.execute(getStarted())
+    cy.visit("/dashboard")
   })
 
-  it("does something")
+  it("creates the first promoter", () => {
+    cy.getByTestId("create-first-promoter-btn").click()
+    cy.getByTestId("promoter-card").contains("Untitled promoter")
+    cy.getByTestId("create-promoter-btn").click()
+    cy.getByTestId("promoter-card").should('have.length', 2)
+  })
 })
