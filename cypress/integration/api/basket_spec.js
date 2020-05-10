32824625f07922e8ce807d178d3dbcf20cc94e6e
James Bowers <james@james-bowers.me>
2020-05-10 14:53:41 +0100

    test fetching items in a basket

--- cypress/integration/api/basket_spec.js
+++ cypress/integration/api/basket_spec.js
@@ -1,4 +1,4 @@
-import {startCheckout, getStarted, signOut, createActivityDate, createBasket, getReservedSeats} from "tb-sdk"
+import {startCheckout, getStarted, signOut, createActivityDate, createBasket, getBasket} from "tb-sdk"
 import {productIdToItemId, ticketSetup} from "../../support/helpers"
 
 beforeEach(() => {
@@ -6,7 +6,6 @@ beforeEach(() => {
 })
 
 describe("Basket", () => {
-
   it("allows you to create a basket", () => {
     cy.execute(getStarted())
     const {createTicketReq} = ticketSetup()
@@ -28,6 +27,37 @@ describe("Basket", () => {
           checkout_id: createBasketReq.body.wish.checkout_id,
           reserved_items: {},
           state: "pending"
+        })
+      })
+  })
+
+  it("fetches items in a basket", () => {
+    cy.execute(getStarted())
+    const {createTicketReq} = ticketSetup()
+    const checkoutAmount = 400
+
+    cy.execute(signOut())
+    cy.execute(getStarted())
+    cy.upgradeToVerified()
+
+    const createBasketReq = createBasket({
+      [createTicketReq.body.wish.product_id]: 1
+    })
+
+    cy.execute(createBasketReq)
+
+    const getBasketReq = getBasket(createBasketReq.body.wish.basket_id)
+    cy.execute(getBasketReq)
+      .then((res) => {
+        expect(res.status).to.eq(200)
+        const itemReserved = Object.values(res.body)[0]
+        expect(itemReserved).to.deep.include({
+          amount: 400,
+          basket_id: createBasketReq.body.wish.basket_id,
+          product_id: createTicketReq.body.wish.product_id,
+          shareholders: {"creditor-one-two-three": 400},
+          status: "reserved",
+          title: "Early bird ticket",
         })
       })
   })
